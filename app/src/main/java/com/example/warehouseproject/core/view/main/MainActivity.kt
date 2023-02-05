8d6a02f84b007083679c19fc117a44097fc036f6
A-fauzi <akhmadfauzi0101@gmail.com>
2023-02-06 01:04:15 +0700

    fix bug scanner support fragment

--- app/src/main/java/com/example/warehouseproject/core/view/main/MainActivity.kt
+++ app/src/main/java/com/example/warehouseproject/core/view/main/MainActivity.kt
@@ -1,36 +1,52 @@
 package com.example.warehouseproject.core.view.main
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
-import android.graphics.Color
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.warehouseproject.R
-import com.example.warehouseproject.core.helper.RandomColor
-import com.example.warehouseproject.core.model.product.Product
-import com.example.warehouseproject.core.view.product.add_product.AddProductActivity
-import com.example.warehouseproject.core.service.product.ProductApiService
 import com.example.warehouseproject.core.view.main.account_fragment.AccountFragment
-import com.example.warehouseproject.core.view.main.home_fragment.HomeAdapter
 import com.example.warehouseproject.core.view.main.home_fragment.HomeFragment
-import com.example.warehouseproject.core.view.main.home_fragment.HomePresenter
-import com.example.warehouseproject.core.view.main.home_fragment.HomeView
 import com.example.warehouseproject.core.view.main.scan_fragment.ScanFragment
 import com.example.warehouseproject.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.zxing.*
+import com.google.zxing.common.HybridBinarizer
+import com.google.zxing.integration.android.IntentIntegrator
+import org.json.JSONException
+import java.io.BufferedInputStream
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStream
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var bottomNavigationView: BottomNavigationView
 
+    private var WRITE_EXTERNAL_STORAGE_PERMISSION_CODE: Int = 1
+    private var READ_EXTERNAL_STORAGE_PERMISSION_CODE: Int = 2
+    private var CAMERA_PERMISSION_CODE: Int = 3
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        checkPermission()
 
         loadFragment(HomeFragment())
         bottomNavigationView = binding.bottomNav
@@ -57,5 +73,56 @@ class MainActivity : AppCompatActivity() {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.container, fragment)
         transaction.commit()
+    }
+
+    private fun checkPermission() {
+        when (PackageManager.PERMISSION_DENIED) {
+            checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) -> {
+                requestPermissions(
+                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    WRITE_EXTERNAL_STORAGE_PERMISSION_CODE
+                )
+            }
+            checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                requestPermissions(
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    READ_EXTERNAL_STORAGE_PERMISSION_CODE
+                )
+            }
+            checkSelfPermission(Manifest.permission.CAMERA) -> {
+                requestPermissions(
+                    arrayOf(Manifest.permission.CAMERA),
+                    CAMERA_PERMISSION_CODE
+                )
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            WRITE_EXTERNAL_STORAGE_PERMISSION_CODE -> if (grantResults.isNotEmpty()) {
+                if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                    Toast.makeText(this, "Anda perlu memberikan semua izin untuk menggunakan aplikasi ini.", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            }
+            READ_EXTERNAL_STORAGE_PERMISSION_CODE -> if (grantResults.isNotEmpty()) {
+                if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                    Toast.makeText(this, "Anda perlu memberikan semua izin untuk menggunakan aplikasi ini.", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            }
+            CAMERA_PERMISSION_CODE -> if (grantResults.isNotEmpty()) {
+                if (grantResults[0] == PackageManager.PERMISSION_DENIED){
+                    Toast.makeText(this, "Anda perlu memberikan semua izin untuk menggunakan aplikasi ini.", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/warehouseproject/core/view/main/scan_fragment/ScanFragment.kt
+++ app/src/main/java/com/example/warehouseproject/core/view/main/scan_fragment/ScanFragment.kt
@@ -1,60 +1,155 @@
 package com.example.warehouseproject.core.view.main.scan_fragment
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import com.example.warehouseproject.R
+import com.example.warehouseproject.databinding.FragmentScanBinding
+import com.google.zxing.*
+import com.google.zxing.common.HybridBinarizer
+import com.google.zxing.integration.android.IntentIntegrator
+import org.json.JSONException
+import java.io.BufferedInputStream
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStream
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ScanFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ScanFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentScanBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scan, container, false)
+        binding = FragmentScanBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ScanFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ScanFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnClickScan.setOnClickListener {
+            clickScan()
+        }
+    }
+
+    private fun clickScan() {
+        val scanOptions = arrayOf("Camera", "Gallery")
+        AlertDialog.Builder(requireActivity())
+            .setTitle("Scan QR Code melalui")
+            .setItems(scanOptions) { _, which-> when (which) {
+                0 -> openCamera()
+                1 -> openGallery()
+            } }
+            .create()
+            .show()
+    }
+    private fun openCamera() {
+        val qrScan = IntentIntegrator.forSupportFragment(this@ScanFragment)
+        qrScan.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+        qrScan.setPrompt("Scan a QR Code")
+        qrScan.setOrientationLocked(true)
+        qrScan.setBeepEnabled(true)
+        qrScan.setBarcodeImageEnabled(true)
+        //initiating the qr code scan
+        qrScan.initiateScan()
+    }
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null)
+        {
+            //if qrcode has nothing in it
+            if (result.contents == null){
+                Toast.makeText(requireActivity(), "Result Not Found", Toast.LENGTH_LONG).show()
+            } else {
+                //if qr contains data
+                try {
+                    val contents = result.contents
+
+                    binding.tvResult.text = contents
+
+                }
+                catch (e: JSONException) {
+                    e.printStackTrace()
+                    //if control comes here
+                    //that means the encoded format not matches
+                    //in this case you can display whatever data is available on the qrcode
+                    //to a toast
+                    Toast.makeText(requireActivity(), result.contents, Toast.LENGTH_LONG).show()
                 }
             }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+    private fun openGallery() {
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        resultLauncherGallery.launch(galleryIntent)
+    }
+
+    var resultLauncherGallery = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+
+            val imageUri = data!!.data!!
+            val imagePath = convertMediaUriToPath(imageUri)
+            val imgFile = File(imagePath)
+            scanImageQRCode(imgFile)
+        } else {
+            Toast.makeText(requireActivity(), "Result Not Found", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun convertMediaUriToPath(uri: Uri):String {
+        val proj = arrayOf<String>(MediaStore.Images.Media.DATA)
+        val cursor = requireActivity().contentResolver.query(uri, proj, null, null, null)
+        val columnIndex = cursor!!.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        cursor.moveToFirst()
+        val path = cursor.getString(columnIndex)
+        cursor.close()
+        return path
+    }
+
+    private fun scanImageQRCode(file: File){
+        val inputStream: InputStream = BufferedInputStream(FileInputStream(file))
+        val bitmap = BitmapFactory.decodeStream(inputStream)
+        val decoded = scanQRImage(bitmap)
+        Log.i("QrTest", "Decoded string=$decoded")
+    }
+
+    private fun scanQRImage(bMap: Bitmap): String? {
+        var contents: String? = null
+        val intArray = IntArray(bMap.width * bMap.height)
+        //copy pixel data from the Bitmap into the 'intArray' array
+        bMap.getPixels(intArray, 0, bMap.width, 0, 0, bMap.width, bMap.height)
+        val source: LuminanceSource = RGBLuminanceSource(bMap.width, bMap.height, intArray)
+        val bitmap = BinaryBitmap(HybridBinarizer(source))
+        val reader: Reader = MultiFormatReader()
+        try {
+            val result: Result = reader.decode(bitmap)
+            contents = result.text
+
+            binding.tvResult.text = contents
+        } catch (e: Exception) {
+            Log.e("QrTest", "Error decoding qr code", e)
+            Toast.makeText(requireActivity(), "Error decoding QR Code, Mohon pilih gambar QR Code yang benar!", Toast.LENGTH_SHORT).show()
+        }
+        return contents
     }
 }
